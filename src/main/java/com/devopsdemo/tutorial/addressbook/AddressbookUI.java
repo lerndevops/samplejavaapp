c340ca5f0e56a4ba467a8b4563688bdf0281ef47
leaddevops <36464863+leaddevops@users.noreply.github.com>
2023-09-16 10:46:39 +0530

    Update AddressbookUI.java

--- src/main/java/com/devopsdemo/tutorial/addressbook/AddressbookUI.java
+++ src/main/java/com/devopsdemo/tutorial/addressbook/AddressbookUI.java
@@ -18,7 +18,7 @@ import com.vaadin.v7.data.util.BeanItemContainer;
 import com.vaadin.v7.ui.Grid;
 import com.vaadin.v7.ui.TextField;
 
-/* User Interface written in Java.
+/** User Interface written in Java.
  *
  * Define the user interface shown on the Vaadin generated web page by extending the UI class.
  * By default, a new UI instance is automatically created when the page is loaded. To reuse
