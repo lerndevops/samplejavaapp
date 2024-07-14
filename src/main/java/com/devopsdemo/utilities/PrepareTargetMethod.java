b44cefc053cf62c800a85d2e6a86bed427f1c99b
leaddevops <36464863+leaddevops@users.noreply.github.com>
2024-07-14 22:44:30 +0530

    Update PrepareTargetMethod.java

--- src/main/java/com/devopsdemo/utilities/PrepareTargetMethod.java
+++ src/main/java/com/devopsdemo/utilities/PrepareTargetMethod.java
@@ -8,7 +8,7 @@ public class PrepareTargetMethod {
 	 *
 	 * @param name a {@link java.lang.String}
 	 * @return methodName a {@link java.lang.String}
-	 */
+	 **/
 	public String prepareTargetMethod(String name) {
 		StringBuffer fieldName = new StringBuffer(METHOD_GET_PREFIX);
 		fieldName.append(name.substring(0, 1).toUpperCase()).append(name.substring(1));
