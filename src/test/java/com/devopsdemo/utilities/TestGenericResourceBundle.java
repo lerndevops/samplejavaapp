dc611f64a6172432f49db58498b3f978699647d6
nareshwart <nareshwart@gmail.com>
2025-07-26 14:11:07 +0530

    fix

--- src/test/java/com/devopsdemo/utilities/TestGenericResourceBundle.java
+++ src/test/java/com/devopsdemo/utilities/TestGenericResourceBundle.java
@@ -1,15 +1,15 @@
 
 package com.devopsdemo.utilities;
-import com.devopsdemo.helper.GenericResourceBundle;
-
-import org.junit.jupiter.api.Test;
+        assertThrows(IllegalArgumentException.class, () -> {
+            GenericResourceBundle.getBundle(null);
+        });
 import java.util.Locale;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TestGenericResourceBundle {
-
-    @Test
-    void testGetBundle() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            GenericResourceBundle.getBundle("nonexistent");
+        });
         Locale locale = new Locale("en", "US");
         assertNotNull(GenericResourceBundle.getBundle("messages", locale));
     }
